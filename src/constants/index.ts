1409f3cd94b26a0da6aea1b7d2e2a4b8677c1518
LuanVo <vtluan.nt.1999@gmail.com>
2024-01-16 23:41:57 +0700

    update: default recaptcha api key

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,5 +5,5 @@ export const ENV: Record<string, string> = {
   locale: env.REACT_APP_I18N_LOCALE || 'en',
   baseUrlApi: env.VITE_API_URL || 'https://api-community.skyglab.tech/',
   websiteUrl: env.VITE_WEBSITE_URL || 'https://apartment-nest-fe.vercel.app/',
-  recaptchaApiKey: env.VITE_RECAPTCHA_API_KEY
+  recaptchaApiKey: env.VITE_RECAPTCHA_API_KEY || '6LdP4FIpAAAAAJH8bOtwC8JcHGEUjmE0135n2671'
 }
